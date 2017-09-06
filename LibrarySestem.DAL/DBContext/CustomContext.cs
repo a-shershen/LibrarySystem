bd425cd84de37ec39e1e306cc68bc8d5ec689929
andrei <accs.shershen@gmail.com>
2017-09-06 11:12:02 +0300

    DAL
    Add DB Initializer

--- LibrarySestem.DAL/DBContext/CustomContext.cs
+++ LibrarySestem.DAL/DBContext/CustomContext.cs
@@ -9,7 +9,12 @@ namespace LibrarySestem.DAL.DBContext
 {
     class CustomContext:DbContext
     {
+         static CustomContext()
+        {
+            Database.SetInitializer<CustomContext>(new DBInitializer());
+        }
         public CustomContext(string connectionString) : base(connectionString) { }
+
 
         public DbSet<Models.Book> Books { get; set; }
 
@@ -18,5 +23,23 @@ namespace LibrarySestem.DAL.DBContext
         public DbSet<Models.ReaderBook> ReaderBooks { get; set; }
 
         public DbSet<Models.User> Users { get; set; }
+    }
+
+    class DBInitializer:System.Data.Entity.DropCreateDatabaseIfModelChanges<CustomContext>
+    {
+        protected override void Seed(CustomContext context)
+        {
+            base.Seed(context);
+
+            context.Users.Add(new Models.User
+            {
+                FirstName = "Ivan",
+                LastName = "Petrov",
+                Login = "admin",
+                //Generate by SHA256 Hash
+                Password = "96cae35ce8a9b0244178bf28e4966c2ce1b8385723a96a6b838858cdd6ca0a1e"
+            });
+            context.SaveChanges();
+        }
     }
 }
